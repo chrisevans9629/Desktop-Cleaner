6880827d02548c9fa5087654979618d027ff2379
Chris Evans <Cevans@peerlessproducts.com>
2019-02-06 06:20:26 -0600

    initial push

--- DesktopCleaner/Program.cs
+++ DesktopCleaner/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace DesktopCleaner
 {
@@ -6,7 +7,86 @@ namespace DesktopCleaner
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var desktopdirectory = $"c:\\users\\{Environment.UserName}\\desktop";
+			var desktopitems = Directory.GetFiles(desktopdirectory);
+			foreach (var item in desktopitems)
+			{
+				var exts = item.Split('.');
+				if (exts.Length > 1)
+				{
+					var ext = exts[exts.Length - 1];
+					if (ext != "lnk")
+					{
+						if (ext == "jpg" || ext == "png")
+						{
+							MoveToNewDirectory(desktopdirectory, item, "photos");
+						}
+						else if (ext == "docx" || ext == "txt" || ext == "log" || ext == "rtf" || ext == "pdf")
+						{
+							MoveToNewDirectory(desktopdirectory, item, "doc");
+						}
+						else if (ext == "cmd" || ext == "ps1" || ext == "bat")
+						{
+							MoveToNewDirectory(desktopdirectory, item, "script");
+						}
+						else if (ext == "csv" || ext == "xls" || ext == "xlsx")
+						{
+							MoveToNewDirectory(desktopdirectory, item, "excel");
+						}
+						else if (ext == "exe" || ext == "msi")
+						{
+							MoveToNewDirectory(desktopdirectory, item, "installer");
+						}
+						else
+						{
+							MoveToNewDirectory(desktopdirectory, item, ext);
+
+						}
+					
+				}
+				}
+
+			}
+			Console.WriteLine("Files Moved!");
+			Console.ReadLine();
+
 		}
-	}
+
+		static void MoveToNewDirectory(string desktopdirectory, string item, string ext)
+		{
+			var directory = desktopdirectory + "\\" + ext + "Files";
+			if (Directory.Exists(directory) != true)
+			{
+				Directory.CreateDirectory(directory);
+			}
+			var names = item.Split('\\');
+			var name = names[names.Length - 1];
+			var newfiledirectory = directory + "\\" + name;
+			var test = CheckForExistingAndChange(newfiledirectory);
+			if (test != null)
+			{
+				newfiledirectory = test;
+			}
+			File.Move(item, newfiledirectory);
+			Console.WriteLine(item + " has been moved to " + directory);
+		}
+		static string CheckForExistingAndChange(string file)
+		{
+			if (File.Exists(file))
+			{
+				FileInfo deskfile = new FileInfo(file);
+				
+				var newname = deskfile.Directory.FullName + "\\" + Path.GetFileNameWithoutExtension(file) + 1 + deskfile.Extension;
+				var testname = CheckForExistingAndChange(newname);
+				if (testname == null)
+				{
+					return newname;
+
+
+				}
+				return testname;
+			}
+			return null;
+		}
+}
 }
